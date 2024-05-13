0f81dadefe3d902a655b6c839ac7fe89818c4860
Fabriciooo <fabricerob@yahoo.fr>
2024-05-13 14:39:20 +0200

    Co-authored-by: Shaykkoo <Shaykkoo@users.noreply.github.com>

---
+++ src/pages/Router.js
@@ -0,0 +1,10 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./HomePage";
+
+export default function Router() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
